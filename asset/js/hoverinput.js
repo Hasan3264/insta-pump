0dc933a0b7a35756efe3180011fa34a09d667074
Hasan3264 <mahmudulhasan4623@gmail.com>
2022-12-15 04:14:58 -0800

    first init

---
+++ asset/js/hoverinput.js
@@ -0,0 +1,9 @@
+
+    $(".input-effect input").val("");		
+		$(".input-effect input").focusout(function(){
+			if($(this).val() != ""){
+				$(this).addClass("has-content");
+			}else{
+				$(this).removeClass("has-content");
+			}
+		});
---
+++ asset/js/script.js
@@ -0,0 +1,40 @@
+
+
+$(function () {
+    'use strict'
+
+   
+
+    var navOff = $('.navigation').offset().top;
+
+    $(window).scroll(function () {
+
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > navOff) {
+            $('.navigation').addClass('manu-fix');
+        } else {
+            $('.navigation').removeClass('manu-fix');
+        }
+    });
+ 
+
+   
+
+    //     //back to top
+    $('.back-to-top').click(function () {
+        $('html,body').animate({
+            scrollTop: 0,
+        });
+    });
+
+    $(window).scroll(function () {
+        var scrolling2 = $(this).scrollTop();
+
+        if (scrolling2 > 150) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut()
+        }
+    });
+});
